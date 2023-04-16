e90029dc2754b541000724b03706d395ba39a01a
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-16 20:48:48 +0500

    dictionary + the following is bigger than the previous

--- урок 3/3.py
+++ урок 3/3.py
@@ -26,3 +26,33 @@ k = 2
 for i in range(k):
     list_b.insert(0, list_b.pop(len(list_b) - 1))
 print(list_b)
+
+
+""" написать программу для печати всех уник значений в словаре
+input [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {"V": "S009"}, {"VIII": "S007"}]
+output: {'S005', 'S002', 'S007', 'S001', 'S009', ' S005 '} """
+
+dictionary = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {"V": "S009"}, {"VIII": "S007"}]
+mnozh = set()
+for i in dictionary:
+    mnozh.add(list(i.values())[0])
+print(mnozh)
+
+
+""" дан массив из целых чисел. подсчитать кол-во элементов массива, больших предыдущего (элемента с предыдущим номером)
+[0, -1, 5, 2, 3]
+2(-1 < 5, 2 < 3) """
+
+import random
+n = int(input("Type the number of the elements: "))
+nums = []
+count = 0
+nums.append(random.randint(-10, 10))
+
+for i in range(n-1):
+    nums.append(random.randint(-10, 10))
+    if nums[i+1] > nums[i]:
+        count += 1
+
+print(nums)
+print(f"The number is {count}")
