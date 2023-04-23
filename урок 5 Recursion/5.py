72393b6a2d7edc03cfd4d11f5eb17d15ce637cd1
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-23 22:56:07 +0500

    more tasks

--- урок 5 Recursion/5.py
+++ урок 5 Recursion/5.py
@@ -12,10 +12,6 @@ print(fib(n))
 
 
 
-
-
-
-
 # Задача 29. Найти факториал числа
 n = int(input("Type the number: "))
 def factorial(n):
@@ -23,4 +19,38 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 
-print(factorial(n))
+print(factorial(n)) 
+
+
+""" Задача No35. Решение в группах
+Напишите функцию, которая принимает одно число и проверяет, является ли оно простым
+Напоминание: Простое число - это число, которое имеет 2 делителя: 1 и n(само число)
+Input: 5 Output: yes """
+
+n = int(input("Type the number: "))
+
+def is_simple(n, t = None):
+    if t is None:
+        t = n - 1
+    while t >= 2:
+        if n % t == 0:
+            return False
+        else:
+            return is_simple(n, t - 1)
+    else:
+        return True
+
+print(is_simple(n))
+
+
+""" Задача 37. Дано натуральное число N и последовательность из N элементов. Требуется вывести эту последовательность в обратном порядке.
+Примечание. В программе запрещается объявлять массивы и использовать циклы (даже для ввода и вывода).
+Input: 2 -> 3 4 Output: 4 3 """
+
+def sequence(n):
+    if n == 0:
+        return "+"
+    i = input("Type the number: ")
+    return sequence(n-1) + i
+
+print(sequence(5))
